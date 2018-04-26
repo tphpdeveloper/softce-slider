22a73d6815903aef70a58b0e2a6e9905e990a274
Igor <ig@c-ua.com>
2018-04-26 15:34:44 +0300

    Removed not needed uri

--- src/routes/web.php
+++ src/routes/web.php
@@ -7,10 +7,6 @@ Route::group([
     'middleware' => ['web']
     ],function(){
 
-//    Route::get('/', ['as' => 'admin.slider', 'uses' => 'SliderController@index']);
-//    Route::post('/create', ['as' => 'admin.slider.create', 'uses' => 'SliderController@create']);
-//    Route::post('/edit', ['as' => 'admin.slider.edit', 'uses' => 'SliderController@create']);
-//    Route::post('/delete', ['as' => 'admin.slider.edit', 'uses' => 'SliderController@create']);
 
     Route::resource( '/slider', 'SliderController', [ 'as' => 'admin', 'only' => ['index', 'store', 'update', 'destroy'] ] );
 
